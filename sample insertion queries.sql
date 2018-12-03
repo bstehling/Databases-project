6b75b9b1bd9bb323ff8b8f969ca76d291bd4247b
Brandon Stehling <44592622+FlyingMongoose2018@users.noreply.github.com>
2018-12-02 20:53:06 -0600

    Update sample insertion queries.sql

--- sample insertion queries.sql
+++ sample insertion queries.sql
@@ -1,7 +1,7 @@
 use recipeMgr;
 
 insert into ing (iname)
-values ('Chicken');
+values ('Noodles');
 
 insert into recipe (rname, time, ins, lunch) 
 values ('Chicken Noodle Soup test', 30, 'This is a test recipe for chicken noodle soup', 1);
@@ -9,7 +9,10 @@ values ('Chicken Noodle Soup test', 30, 'This is a test recipe for chicken noodl
 insert into recipe (rname, time, ins, breakfast) 
 values ('Scrambled Eggs', 10, 'how to make scrambled eggs.....', 1);
 
+insert into ingList (r_id, i_id)
+values (1,4);
+
 SELECT 
     *
 FROM
-    recipe;
+    ingList;
